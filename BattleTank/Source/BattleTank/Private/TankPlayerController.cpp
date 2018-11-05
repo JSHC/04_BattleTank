d7d9e23b9ea7aca91b0e81bfa0a730a024e0685a
Johan Hansson <johan.hansson.carlberg@gmail.com>
2018-11-05 20:32:30 +0100

    Added BeginPlay code in TankPlayerController and code for logging the Tank that is possesed by the player

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,22 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* PlayerTank = GetControlledTank();
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player tank: %s"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player tank: %s"), *PlayerTank->GetName());
+	}
+	
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
