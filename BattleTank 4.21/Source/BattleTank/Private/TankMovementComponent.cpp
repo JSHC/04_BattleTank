400b0b179a7fe9bca030cf30564d21f99c5b73e4
Johan Hansson <johan.hansson.carlberg@gmail.com>
2018-11-27 04:54:19 +0100

    Implemented AI turning

--- BattleTank 4.21/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank 4.21/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -36,8 +36,12 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	auto Name = GetOwner()->GetName();
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
 	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveForward(ForwardThrow);
+
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
 
 	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to: %s"), *Name, *AIForwardIntention.ToString())
 }
